1012ccff4b0d03f169c7d0921cc82ee06df642b2
Unknown <watemerald@gmail.com>
2017-05-12 03:23:53 +0300

    Fixed graphics persisting between iterations

--- GA_Lab/GA/plotting.py
+++ GA_Lab/GA/plotting.py
@@ -1,6 +1,7 @@
 # from bokeh.plotting import figure, output_file, show, save
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 import numpy as np
 
 
@@ -14,7 +15,10 @@ def plot(func, dots, values,filename, min=0, max=1):
     # # show(p)
     # save(p)
     x = np.linspace(min, max, 500)
-    plt.plot(x, func(x))
+    plt.figure()
+    sns.set()
+    sns.set_style('whitegrid')
+    plt.plot(x, func(x), color='b')
     plt.scatter(dots, values, color='r')
 
     plt.savefig('Images/{}'.format(filename))
